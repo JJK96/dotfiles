723172f2d483e663219665045846c3e3dc2e0acc
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2019-07-01 15:36:10 +0200

    And a knop under two branches of the same, and a knop under two branches of the same, and a knop under two branches of the same, according to the six branches going out of it. 		-- Exodus 37:21

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
+ln -sf ~/Pictures/BSOL.png ~/Pictures/active_lockscreen
 ~/.screenlayout/refresh.sh
--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output HDMI-2 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-2 --off
+ln -sf ~/Pictures/bsol_1080pwide.png ~/Pictures/active_lockscreen
 ~/.screenlayout/refresh.sh
