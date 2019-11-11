38ec0dbec798314b109591b316a91f8b4f076ea0
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2019-11-11 18:37:10 +0100

    Make F1 display the right symbols

--- .config/krabby/config.js
+++ .config/krabby/config.js
@@ -1,11 +1,13 @@
 // Configuration for Krabby (https://github.com/alexherbo2/krabby/blob/master/src/krabby.js)
 
-/*Modal.KEY_MAP = () => {
+Modal.KEY_MAP = () => {
+    return {
       Backquote: { key: '`', shiftKey: '~' }, Digit1: { key: '1', shiftKey: '!' }, Digit2: { key: '2', shiftKey: '@' }, Digit3: { key: '3', shiftKey: '#' }, Digit4: { key: '4', shiftKey: '$' }, Digit5: { key: '5', shiftKey: '%' }, Digit6: { key: '6', shiftKey: '^' }, Digit7: { key: '7', shiftKey: '&' }, Digit8: { key: '8', shiftKey: '*' }, Digit9: { key: '9', shiftKey: '(' }, Digit0: { key: '0', shiftKey: ')' }, Minus: { key: '-', shiftKey: '_' }, Equal: { key: '=', shiftKey: '+' },
-      KeyQ: { key: 'q', shiftKey: 'Q' }, KeyW: { key: 'w', shiftKey: 'W' }, KeyE: { key: 'e', shiftKey: 'E' }, KeyR: { key: 'r', shiftKey: 'R' }, KeyT: { key: 't', shiftKey: 'T' }, KeyY: { key: 'y', shiftKey: 'Y' }, KeyU: { key: 'u', shiftKey: 'U' }, KeyI: { key: 'i', shiftKey: 'I' }, KeyO: { key: 'o', shiftKey: 'O' }, KeyP: { key: 'p', shiftKey: 'P' }, BracketLeft: { key: '[', shiftKey: '{' }, BracketRight: { key: ']', shiftKey: '}' }, Backslash: { key: '\\', shiftKey: '|' },
-      KeyA: { key: 'a', shiftKey: 'A' }, KeyS: { key: 's', shiftKey: 'S' }, KeyD: { key: 'd', shiftKey: 'D' }, KeyF: { key: 'f', shiftKey: 'F' }, KeyG: { key: 'g', shiftKey: 'G' }, KeyH: { key: 'h', shiftKey: 'H' }, KeyJ: { key: 'j', shiftKey: 'J' }, KeyK: { key: 'k', shiftKey: 'K' }, KeyL: { key: 'l', shiftKey: 'L' }, Semicolon: { key: ';', shiftKey: ':' }, Quote: { key: "'", shiftKey: '"' },
-      KeyZ: { key: 'z', shiftKey: 'Z' }, KeyX: { key: 'x', shiftKey: 'X' }, KeyC: { key: 'c', shiftKey: 'C' }, KeyV: { key: 'v', shiftKey: 'V' }, KeyB: { key: 'b', shiftKey: 'B' }, KeyN: { key: 'n', shiftKey: 'N' }, KeyM: { key: 'm', shiftKey: 'M' }, Comma: { key: ',', shiftKey: '<' }, Period: { key: '.', shiftKey: '>' }, Slash: { key: '/', shiftKey: '?' }
-}*/
+      KeyQ: { key: 'q', shiftKey: 'Q' }, KeyW: { key: 'w', shiftKey: 'W' }, KeyE: { key: 'f', shiftKey: 'F' }, KeyR: { key: 'p', shiftKey: 'P' }, KeyT: { key: 'g', shiftKey: 'G' }, KeyY: { key: 'j', shiftKey: 'J' }, KeyU: { key: 'l', shiftKey: 'L' }, KeyI: { key: 'u', shiftKey: 'U' }, KeyO: { key: 'y', shiftKey: 'Y' }, KeyP: { key: ';', shiftKey: ':' }, BracketLeft: { key: '[', shiftKey: '{' }, BracketRight: { key: ']', shiftKey: '}' }, Backslash: { key: '\\', shiftKey: '|' },
+      KeyA: { key: 'a', shiftKey: 'A' }, KeyS: { key: 'r', shiftKey: 'R' }, KeyD: { key: 's', shiftKey: 'S' }, KeyF: { key: 't', shiftKey: 'T' }, KeyG: { key: 'd', shiftKey: 'D' }, KeyH: { key: 'h', shiftKey: 'H' }, KeyJ: { key: 'n', shiftKey: 'N' }, KeyK: { key: 'e', shiftKey: 'E' }, KeyL: { key: 'i', shiftKey: 'I' }, Semicolon: { key: 'o', shiftKey: 'O' }, Quote: { key: "'", shiftKey: '"' },
+      KeyZ: { key: 'z', shiftKey: 'Z' }, KeyX: { key: 'x', shiftKey: 'X' }, KeyC: { key: 'c', shiftKey: 'C' }, KeyV: { key: 'v', shiftKey: 'V' }, KeyB: { key: 'b', shiftKey: 'B' }, KeyN: { key: 'k', shiftKey: 'K' }, KeyM: { key: 'm', shiftKey: 'M' }, Comma: { key: ',', shiftKey: '<' }, Period: { key: '.', shiftKey: '>' }, Slash: { key: '/', shiftKey: '?' }
+}
+}
 
 Hint.KEY_MAP = () => {
     return {
