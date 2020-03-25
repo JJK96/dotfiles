ffc971a0f59dac5cfb4c15b3ec64a869dcd44c21
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-03-25 11:20:35 +0100

    Adapt screenlayouts for dock

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI2 --off
+xrandr --output eDP1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI2 --off
 ln -sf ~/Pictures/bsol_1080pwide.png ~/Pictures/active_lockscreen
 ~/.screenlayout/refresh.sh
--- .screenlayout/home_one_screen.sh
+++ .screenlayout/home_one_screen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI2 --off
+xrandr --output eDP1 --off --output DP1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DP2 --off --output HDMI2 --off
 ln -sf ~/Pictures/bsol_1080pwide.png ~/Pictures/active_lockscreen
 ~/.screenlayout/refresh.sh
