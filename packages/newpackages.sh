ee5bc99665603ded3404105d4bfb984a54b367ec
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-04-15 14:41:14 +0200

    Processed new packages

--- packages/newpackages.sh
+++ packages/newpackages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$(mktemp -d)
 cat packages | perl -ne 'print unless (/^$/ || /^#/)' | awk '{print $1}' > $dir/a
-cat ignored_packages/* >> $dir/a
+cat non_essential/* | perl -ne 'print unless (/^$/ || /^#/)' | awk '{print $1}' >> $dir/a
 sort $dir/a > $dir/aa
 pacman -Qe | awk '{print $1}' | sort > $dir/b
 comm -23 $dir/b groups > $dir/c
