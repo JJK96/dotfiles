4366128caef8194863fc7a6efd03f04acb2f1043
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-06-05 10:08:47 +0200

    Other workspace

---
+++ .screenlayout/4k_4k_1080.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 5120x0 --rotate normal --output DP1 --off --output DP2 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
