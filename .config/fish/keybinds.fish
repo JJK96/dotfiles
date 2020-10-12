7d2cdbaa1d8bb7a769498c8680be31857e5c56e0
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-10-12 11:58:24 +0200

    Set up and down keybinds fish

--- .config/fish/keybinds.fish
+++ .config/fish/keybinds.fish
@@ -4,8 +4,8 @@ set -g fish_key_bindings fish_vi_key_bindings
 bind -M insert \en forward-char # accept suggestion
 
 # vi mode
-bind "n" down-line
-bind "e" up-line
+bind "n" down-or-search
+bind "e" up-or-search
 bind "i" forward-char
 bind "j" repeat-jump
 bind "k" forward-word
