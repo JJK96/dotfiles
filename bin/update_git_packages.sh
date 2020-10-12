44dc7c81b5d1e677476e26f6be61d2accc226027
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-10-12 12:21:45 +0200

    Compile alacritty from own fork

--- bin/update_git_packages.sh
+++ bin/update_git_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # not included:
 # - neomutt
-git_packages=( kakoune kak-lsp )
+git_packages=( kakoune kak-lsp alacritty )
 for pkgname in ${git_packages[*]}; do
     cd $HOME/git/$pkgname-git/src/$pkgname
     git pull upstream master || exit
