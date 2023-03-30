e783021f4a9a2a864f724a689dc886c03698550a
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-03-30 13:22:05 +0200

    Add text only flag and better error handling

--- python_bin/extract_js.py
+++ python_bin/extract_js.py
@@ -3,12 +3,17 @@ from burp import get_items
 
 previous_scripts = set()
 
-def scripts_item(item):
+def scripts_generator(item):
     scripts = item.scripts
     if not scripts:
         return None
     scripts_text = []
     for script in scripts:
+        yield script
+
+
+def scripts_item(item):
+    for script in scripts_generator(item):
         script_text = str(script)
         if script_text not in previous_scripts:
             previous_scripts.add(script_text)
@@ -25,21 +30,41 @@ def scripts_item(item):
 
 def get_script_items(input):
     for item in get_items(input):
-        new_item = scripts_item(item)
-        if new_item:
-            yield new_item
+        try:
+            new_item = scripts_item(item)
+            if new_item:
+                yield new_item
+        except Exception as e:
+            print("Exception while processing {}: {}".format(item.url, e))
+            continue
+
+def get_scripts_text(input):
+    for item in get_items(input):
+        try:
+            for script in scripts_generator(item):
+                yield script.text
+        except Exception as e:
+            print("Exception while processing {}: {}".format(item.url, e))
+            continue
+                
 
 
-def extract_js(input, output):
-    script_items = get_script_items(input)
-    with open(output, 'w') as f:
-        string = "<items>\n" + "\n".join(script_items) + "</items>"
-        f.write(string)
+def extract_js(input, output, text_only=False):
+    if text_only:
+        scripts = get_scripts_text(input)
+        with open(output, 'w') as f:
+            f.write("\n".join(scripts))
+    else:
+        script_items = get_script_items(input)
+        with open(output, 'w') as f:
+            string = "<items>\n" + "\n".join(script_items) + "</items>"
+            f.write(string)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser("Extract JavaScript from burp export")
     parser.add_argument("input_file", help="Export from burp items")
     parser.add_argument("output_file", help="Output file")
+    parser.add_argument("--text-only", action="store_true", help="Output only the contents of each <script> tag")
     args = parser.parse_args()
-    extract_js(args.input_file, args.output_file)
+    extract_js(args.input_file, args.output_file, args.text_only)
