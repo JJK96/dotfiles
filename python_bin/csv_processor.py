985b75b570864569b4d77d8723c9f07d84b710e5
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-10-20 08:41:11 +0200

    Add delimiter support

--- python_bin/csv_processor.py
+++ python_bin/csv_processor.py
@@ -1,10 +1,10 @@
 import csv
 
 
-def process_csv(input_file, output_file, process_function):
+def process_csv(input_file, output_file, process_function, delimiter=','):
     with open(input_file) as input, open(output_file, 'w') as output:
-        reader = csv.DictReader(input)
-        writer = csv.DictWriter(output, fieldnames=reader.fieldnames)
+        reader = csv.DictReader(input, delimiter=delimiter)
+        writer = csv.DictWriter(output, fieldnames=reader.fieldnames, delimiter=delimiter)
         writer.writeheader()
         for line in reader:
             if process_function(line):
