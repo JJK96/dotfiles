a2916b3647f617e8cde32134375544ccb2b3424f
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-11-13 12:29:23 +0100

    Allow processing streams instead of files

--- python_bin/csv_processor.py
+++ python_bin/csv_processor.py
@@ -1,14 +1,18 @@
 import csv
 
 
+def process_csv_streams(input, output, process_function, delimiter=','):
+    reader = csv.DictReader(input, delimiter=delimiter)
+    writer = csv.DictWriter(output, fieldnames=reader.fieldnames, delimiter=delimiter)
+    writer.writeheader()
+    for line in reader:
+        if process_function(line):
+            writer.writerow(line)
+
+
 def process_csv(input_file, output_file, process_function, delimiter=','):
     with open(input_file) as input, open(output_file, 'w') as output:
-        reader = csv.DictReader(input, delimiter=delimiter)
-        writer = csv.DictWriter(output, fieldnames=reader.fieldnames, delimiter=delimiter)
-        writer.writeheader()
-        for line in reader:
-            if process_function(line):
-                writer.writerow(line)
+        process_csv_streams(input, output, process_function, delimiter=delimiter)
 
 
 def csv_lines(input_file, delimiter=','):
